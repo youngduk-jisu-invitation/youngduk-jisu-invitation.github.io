6aacb844c3333897cba657e73345991feed632d9
jisu_oh <jisu_oh@tmax.co.kr>
2026-03-07 09:53:27 +0900

    [fix] 구절 변경

--- src/pages/Quote.svelte
+++ src/pages/Quote.svelte
@@ -5,7 +5,7 @@
     <div class="quote-content">
         <p class="quote-text">"삶의 가장 아름다운 순간을 함께 합니다."</p>
         <p class="quote-author">영덕 & 지수</p>
-        <p class="scripture">
+        <!-- <p class="scripture">
             사랑은 오래 참고, 친절합니다. 사랑은 시기하지 않고,<br /> 뽐내지 않으며 교만하지
             않습니다.
             <br />
@@ -17,11 +17,11 @@
             사랑은 모든 것을 덮어 주고, 변함없이 항상 믿어 주며, <br />언제 어디서나 소망을 품고,
             모든 것을 견딥니다.
         </p>
-        <p class="scripture-source">(쉬운성경) 고린도전서 13장 4~7절</p>
-    </div>
-    <div class="quote-image">
-        <div class="image-info">
-            <img src="/images/quote.jpeg" alt="구절" />
+        <p class="scripture-source">(쉬운성경) 고린도전서 13장 4~7절</p> -->
+        <div class="quote-image">
+            <div class="image-info">
+                <img src="/images/quote2.jpg" alt="구절" />
+            </div>
         </div>
     </div>
 </section>
@@ -69,7 +69,6 @@
     }
     
     .quote-image {
-        display: none;
         width: 100%;
         max-width: 350px;
     }
